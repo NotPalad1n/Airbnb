a4be947b35fc08305faec2b4f82568df2f1fe63a
unknown <younesoumast2@gmail.com>
2023-06-11 23:23:35 +0100

    updated

---
+++ src/Data/CardData.jsx
@@ -0,0 +1,29 @@
+export default [
+    {
+        img:require("../images/img1.png"),
+        status:"SOLD OUT",
+        stars:"5.0",
+        reviews:6,
+        location:"USA",
+        title:"Life lessons with Katie Zaferes",
+        price:136
+    },
+    {
+        img:require("../images/img2.png"),
+        status:"ONLINE",
+        stars:"5.0",
+        reviews:"30",
+        location:"USA",
+        title:"Learn wedding photography",
+        price:"126"
+    },
+    {
+        img:require("../images/img3.png"),
+    
+        stars:"4.8",
+        reviews:"2",
+        location:"USA",
+        title:"Group Mountain Biking",
+        price:"50"
+    }
+]
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,37 +3,23 @@ import React from 'react'
 import "../styling/List.css"
 
 import Card from './Card'
+import CardData from '../Data/CardData'
 
 const List = () => {
+  
+  const Cards = CardData.map(card => <Card 
+    img={card.img}
+    status={card.status}
+    stars={card.stars}
+    reviews={card.reviews}
+    location={card.location}
+    title={card.title}
+    price={card.price}
+  />)
+  
   return (
     <div className='list'>
-      <Card
-        img={require("../images/img1.png")}
-        status="SOLD OUT"
-        stars="5.0"
-        reviews={6}
-        location="USA"
-        title="Life lessons with Katie Zaferes"
-        price="136"
-      />
-      <Card
-        img={require("../images/img2.png")}
-        status="ONLINE"
-        stars="5.0"
-        reviews={30}
-        location="USA"
-        title="Learn wedding photography"
-        price="126"
-      />
-      <Card
-        img={require("../images/img3.png")}
-        
-        stars="4.8"
-        reviews={2}
-        location="USA"
-        title="Group Mountain Biking"
-        price="50"
-      />
+      {Cards}
     </div>
   )
 }
